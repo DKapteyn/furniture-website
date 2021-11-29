ff74bd1e869c96321dae1d3f0b125af874c352ea
David Kapteyn <dave.kapteyn@gmail.com>
2021-11-28 22:54:23 -0700

    style improvment and changes to navbar

--- src/nav.js
+++ src/nav.js
@@ -1,5 +1,8 @@
 
 export default function Nav(){
+
+  
+
 return(
 <div className="nav">
 <div className="nav--image-title">
@@ -7,10 +10,14 @@ return(
 <nav className="nav--Title">room</nav>
 </div>
 <ul className="nav--ul">
+
+<img  src="/images/icon-close.svg" alt="x"  className="nav--image" />
+
 <li className="nav--List">home</li>
 <li className="nav--List">shop</li>
 <li className="nav--List">about</li>
 <li className="nav--List">contact</li>
+
 </ul>
 </div>
 )
