b468900ffb2b2272b1d3100201c516c1321fc510
DKapteyn <86522409+DKapteyn@users.noreply.github.com>
2022-01-24 16:21:08 -0700

    animation added through framer motion node modules added to git ignore

--- src/main.js
+++ src/main.js
@@ -1,17 +1,58 @@
-
+import { motion } from "framer-motion";
 
 export default function Main(props) {
-       
-  
-  
+  const variant = {
+    hidden: { opacity: 0.2 },
+    visible: { opacity: 1 },
+    transition: { duration: 0.7 },
+  };
+
   return (
     <div className="main--container">
-      <img className= "main--large-heropic" src={props.Limg} alt="hero pic" />
-      <img className={`main--small-heropic${props.classSize}`} src={props.Simg} alt="hero pic" />
+      <motion.img
+        initial="hidden"
+        animate="visible"
+        transition="transition"
+        variants={variant}
+        key={props.Limg}
+        className="main--large-heropic"
+        src={props.Limg}
+        alt="hero pic"
+      />
+      <motion.img
+        initial="hidden"
+        animate="visible"
+        transition="transition"
+        variants={variant}
+        key={props.Simg}
+        className={`main--small-heropic${props.classSize}`}
+        src={props.Simg}
+        alt="hero pic"
+      />
       <div className={`main--rightcontainer${props.classSize}`}>
         <div className={`main--righttext`}>
-          <h1 className="main--title"> {props.title}</h1>
-          <p className={`main--para${props.classSize}`}> {props.para}</p>
+          <motion.h1
+            initial="hidden"
+            animate="visible"
+            transition="transition"
+            variants={variant}
+            key={props.title}
+            className="main--title"
+          >
+            {" "}
+            {props.title}
+          </motion.h1>
+          <motion.p
+            initial="hidden"
+            animate="visible"
+            transition="transition"
+            variants={variant}
+            key={props.Limg}
+            className={`main--para${props.classSize}`}
+          >
+            {" "}
+            {props.para}
+          </motion.p>
           <div className="main--shopnow">
             <span>SHOP NOW</span>
             <img className="arrow" src="\images\icon-arrow.svg" alt="arrow" />
@@ -19,11 +60,18 @@ export default function Main(props) {
         </div>
         <div className="button-container">
           <div onClick={props.handleClickDown} className={`leftbutton, button`}>
-            
-            <img className={`angleImage${props.classSize}`} src="\images\icon-angle-left.svg" alt="left pointer" />
+            <img
+              className={`angleImage${props.classSize}`}
+              src="\images\icon-angle-left.svg"
+              alt="left pointer"
+            />
           </div>
           <div onClick={props.handleClickUp} className={`rightbutton, button`}>
-            <img className={`angleImage${props.classSize}`} src="\images\icon-angle-right.svg" alt="right pointer" />
+            <img
+              className={`angleImage${props.classSize}`}
+              src="\images\icon-angle-right.svg"
+              alt="right pointer"
+            />
           </div>
         </div>
       </div>
